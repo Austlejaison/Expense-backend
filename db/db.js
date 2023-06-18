ddb9450b0dafb48cf17ef765d5b11024981a26ee
Austlejaison <109846481+Austlejaison@users.noreply.github.com>
2023-06-18 18:21:34 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,26 +1,13 @@
+const mongoose = require('mongoose');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://austlejaison555:Messi10@cluster0.4smd8wg.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Db Connected')
+    } catch (error) {
+        console.log('DB Connection Error',error);
+    }
 }
-run().catch(console.dir);
+
+module.exports = {db}
